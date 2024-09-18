2938f6f45fe499a0115e3ca5bcf5ee900eae1a87
Maxine1425 <105820818+Maxine1425@users.noreply.github.com>
2024-09-18 14:35:01 +0200

    Add files via upload
    enlevé accent de créer + modifier nom de la bdd

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ session_start();
 $host='localhost';
 $user = "root";
 $password = "l'ec'efmtnc'ePD33";
-$base = "bddprojet";
+$base = "projet_cybermacols";
 
 $link = connexion_MySQLi_procedural($host, $user,$password,$base);
 
