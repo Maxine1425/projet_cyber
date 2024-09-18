99e47bfe8de18bf5721643817a7c01ebfa5b4807
ColineTournillon <132445584+ColineTournillon@users.noreply.github.com>
2024-09-18 14:01:53 +0200

    Add files via upload
    Même commit que le précédent car les fichiers étaient vides

---
+++ blog.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <nav>
+        <h1> Blog Cyber</h1>
+        <div class="onglets">
+            <a href="#">Home</a>
+            <a href="#">Articles</a>
+        </div>
+    </nav>
+
+    <section class="articles">
+        <div class="article">
+            <div class="left">
+                <img src="">
+            </div>
+            <div class="right">
+                <p class="date"></p>
+                <p class="heure"></p>
+                <h1></h1>
+                <p class="description"></p>
+                <p class="auteur"></p>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
