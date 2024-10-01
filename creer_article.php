988fc20d3fd2e7ec91b39b588f8d1ad0a94b2fde
Maxine1425 <105820818+Maxine1425@users.noreply.github.com>
2024-10-01 15:51:55 +0200

    Add files via upload
    modification de la fonction uploader, on peut mettre une image dans la bdd, il faut corriger les requête sql pour mettre une image en meme temps que l'article et pas indépendamment.
    Corection des guimmet dans la requeste sql pour enlever des erreurs.

--- creer_article.php
+++ creer_article.php
@@ -2,63 +2,20 @@
     include ("config.php");
     EstConnecte();
     $id_compte=$_SESSION['id_compte'];
-    $ret        = false;
-    $img_blob   = '';
-    $img_taille = 0;
-    $img_type   = '';
-    $img_nom    = '';
-    $taille_max = 250000;
-    $ret        = is_uploaded_file($_FILES['image']['tmp_name']);
-        
-    if (!$ret)
-    {
-        echo "Problème de transfert";
-        return false;
-    } 
-    else 
-    {
-        // Le fichier a bien été reçu
-        $img_taille = $_FILES['image']['size'];
-            
-        if ($img_taille > $taille_max) 
-        {
-            echo "Fichier trop volumineux";
-            return false;
-        }
-
-        $img_type = $_FILES['image']['type'];
-        $img_nom  = $_FILES['image']['name'];
-        $auteur=GetSQLValue("select pseudo from compte where id_compte ='$id_compte'");
-        $img_blob = file_get_contents ($_FILES['name']['tmp_name']);
-        $sql = "INSERT INTO article WHERE auteur ='$auteur' ('image') VALUES ("addslashes ($img_blob)") ";
-        $ret = mysql_query ($sql) or die (mysql_error ());
-        return true;
-        }
     
-    if (isset($_POST["image"])) 
+    if (isset($_POST["nom_article"])) 
     {
-        $image = QuoteStr($_POST["image"]);
+        $image = QuoteStr($_POST["auteur"]);
         $nom_article = QuoteStr($_POST["nom_article"]);
         $contenu = QuoteStr($_POST["contenu"]);
         $auteur=GetSQLValue("select pseudo from compte where id_compte ='$id_compte'");
-        $sql = "insert into article (auteur,nom_article,contenu,image) values ('$auteur','$nom_article','$contenu','$image')";
+        $sql = "insert into article (auteur, nom_article, contenu) values ('$auteur', $nom_article, $contenu, $image)";
 
+        echo $sql;  // Débogage
+        echo 'coucou';
         ExecuteSQL($sql);
     }
-
-    
-
-    //$auteur=GetSQLValue("select pseudo from compte where id_compte ='$id_compte'");
-    //$sql = "insert into article (auteur,nom_article,contenu,image) values ('$auteur','$nom_article','$contenu','$image')";
-
-
-    /*if (!empty($nom_article) && !empty($contenu)) 
-    {
-        ExecuteSQL($sql);
-        
-        //htmlspecialchars(header("location: blog.php"));
-    }*/
-     
+ 
 ?>
 
 
@@ -85,11 +42,13 @@
     <input type="submit" value="Valider">
     </form>
     <h2>Uploader une image</h2>
-    <form action="creer_article.php" method="POST" enctype="multipart/form-data">
+    
+    <form action="upload.php" method="POST" enctype="multipart/form-data">
         <label for="image">Choisissez une image :</label>
         <input type="file" name="image">
         <button type="submit" name="upload">Uploader</button>
     </form>
+
 </body>
 </html>
 <?php mysqli_close($link) ?>
--- upload.php
+++ upload.php
@@ -1,37 +1,41 @@
 <?php
     include ("config.php");
     EstConnecte();
-    $id_compte=$_SESSION['id_compte'];
-    $ret        = false;
-    $img_blob   = '';
+    $id_compte = $_SESSION['id_compte'];
+    $ret       = false;
+    $img_blob  = '';
     $img_taille = 0;
-    $img_type   = '';
-    $img_nom    = '';
+    $img_type  = '';
+    $img_nom   = '';
     $taille_max = 250000;
-    $ret        = is_uploaded_file($_FILES['image']['tmp_name']);
-        
-    if (!$ret)
-    {
+    
+    $ret = is_uploaded_file($_FILES['image']['tmp_name']);
+    
+    if (!$ret) {
         echo "Problème de transfert";
         return false;
-    } 
-    else 
-    {
+    } else {
         // Le fichier a bien été reçu
         $img_taille = $_FILES['image']['size'];
-            
-        if ($img_taille > $taille_max) 
-        {
+        
+        if ($img_taille > $taille_max) {
             echo "Fichier trop volumineux";
             return false;
         }
 
         $img_type = $_FILES['image']['type'];
         $img_nom  = $_FILES['image']['name'];
-        $auteur=GetSQLValue("select pseudo from compte where id_compte ='$id_compte'");
-        $img_blob = file_get_contents ($_FILES['name']['tmp_name']);
-        $sql = "INSERT INTO article WHERE auteur ='$auteur' ('image') VALUES ("addslashes ($img_blob)") ";
-        $ret = mysql_query ($sql) or die (mysql_error ());
+        $auteur   = GetSQLValue("SELECT pseudo FROM compte WHERE id_compte ='$id_compte'");
+        
+        // Correction : Utilisation de 'image' comme clé
+        $img_blob = file_get_contents($_FILES['image']['tmp_name']);
+        
+        // Correction de l'insertion avec 'mysqli_query'
+        $sql = "INSERT INTO article (auteur, image) VALUES ('$auteur', '" . addslashes($img_blob) . "')";
+        
+        // Utilisation de mysqli_query à la place de mysql_query
+        $ret = mysqli_query($link, $sql) or die(mysqli_error($link));
+        
         return true;
-        }
+    }
 ?>
