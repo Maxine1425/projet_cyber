237e8c124bdb75fb30c4dbff81ed6dfd1b0b9c1f
Maxine1425 <105820818+Maxine1425@users.noreply.github.com>
2024-10-02 14:15:04 +0200

    Add files via upload
    test upload

--- upload.php
+++ upload.php
@@ -1,14 +1,14 @@
 <?php
     include ("config.php");
     EstConnecte();
-    $id_article = $_SESSION['id_article'];
-   // $id_compte = $_SESSION['id_compte'];
+    //$id_article = $_SESSION['id_article'];
+    $id_compte = $_SESSION['id_compte'];
     $ret       = false;
     $img_blob  = '';
     $img_taille = 0;
     $img_type  = '';
     $img_nom   = '';
-    $taille_max = 250000;
+    $taille_max = 500000;
     
     $ret = is_uploaded_file($_FILES['image']['tmp_name']);
     
@@ -26,13 +26,13 @@
 
         $img_type = $_FILES['image']['type'];
         $img_nom  = $_FILES['image']['name'];
-        //$auteur   = GetSQLValue("SELECT pseudo FROM compte WHERE id_compte ='$id_compte'");
+        $auteur   = GetSQLValue("SELECT pseudo FROM compte WHERE id_compte ='$id_compte'");
         
         // Correction : Utilisation de 'image' comme clé
         $img_blob = file_get_contents($_FILES['image']['tmp_name']);
         
         // Correction de l'insertion avec 'mysqli_query'
-        $sql = "update article set image='" . addslashes($img_blob) . "' where id_article='$id_article'";
+        $sql = "update article set image='" . addslashes($img_blob) . "' where auteur='$auteur'";
         
         // Utilisation de mysqli_query à la place de mysql_query
         $ret = mysqli_query($link, $sql) or die(mysqli_error($link));
