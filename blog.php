5234f44413e0800c693efebe6a10db99077fe927
Maxine1425 <105820818+Maxine1425@users.noreply.github.com>
2024-10-02 16:43:54 +0200

    Add files via upload
    debut de afficher les articles sur le blog

--- blog.php
+++ blog.php
@@ -2,6 +2,9 @@
     include ("config.php");
     EstConnecte();
 
+    // Récupérer tous les articles de la base de données
+    $sql = "SELECT * FROM article ORDER BY date_creation DESC";
+    $result = mysqli_query($link, $sql);
 ?>
 
 <!DOCTYPE html>
@@ -22,18 +25,36 @@
     </nav>
 
     <section class="articles">
+        <?php
+        // Vérifier si des articles ont été trouvés
+        if (mysqli_num_rows($result) > 0) {
+            // Parcourir chaque article
+            while ($article = mysqli_fetch_assoc($result)) {
+                // Si une image est présente, la convertir en base64 pour l'affichage
+                $image = $article['image'] ? 'data:image/jpeg;base64,' . base64_encode($article['image']) : 'default_image.jpg'; // Remplacez par l'image par défaut si pas d'image
+                $date = date('d/m/Y', strtotime($article['date_creation']));
+                $heure = date('H:i', strtotime($article['date_creation']));
+        ?>
         <div class="article">
             <div class="left">
-                <img src="">
+                <img src="<?php echo $image; ?>" alt="Image de l'article">
             </div>
             <div class="right">
-                <p class="date"></p>
-                <p class="heure"></p>
-                <h1></h1>
-                <p class="description"></p>
-                <p class="auteur"></p>
+                <p class="date"><?php echo $date; ?></p>
+                <p class="heure"><?php echo $heure; ?></p>
+                <h1><?php echo htmlspecialchars($article['nom_article']); ?></h1>
+                <p class="description"><?php echo htmlspecialchars($article['contenu']); ?></p>
+                <p class="auteur"><?php echo htmlspecialchars($article['auteur']); ?></p>
             </div>
         </div>
+        <?php
+            }
+        } else {
+            echo "<p>Aucun article n'a été trouvé.</p>";
+        }
+        ?>
     </section>
 </body>
 </html>
+
+<?php mysqli_close($link); ?>
