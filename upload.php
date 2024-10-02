902634ce33ac7472d75b1c9244d2aca111b63d77
ColineTournillon <132445584+ColineTournillon@users.noreply.github.com>
2024-10-02 16:16:50 +0200

    Update upload.php
    modifications mineures

--- upload.php
+++ upload.php
@@ -16,41 +16,45 @@
         $nom_article = QuoteStr($_POST["nom_article"]);
         $contenu = QuoteStr($_POST["contenu"]);
         $auteur=GetSQLValue("select pseudo from compte where id_compte ='$id_compte'");
-        $sql1 = "insert into article (auteur, nom_article, contenu) values ('$auteur', $nom_article, $contenu)";
+        $sql = "insert into article (auteur, nom_article, contenu) values ('$auteur', $nom_article, $contenu)";
 
-        echo $sql1;  // Débogage
+        echo $sql;  // Débogage
         echo 'coucou';
         $ret = is_uploaded_file($_FILES['image']['tmp_name']);
-        if (!$ret) {
+        if (!$ret) 
+        {
         echo "Problème de transfert";
         return false;
-        } else {
+        } 
+        else 
+        {
         // Le fichier a bien été reçu
-        $img_taille = $_FILES['image']['size'];
+            $img_taille = $_FILES['image']['size'];
         
-        if ($img_taille > $taille_max) {
-            echo "Fichier trop volumineux";
-            return false;
-        }
-
-        $img_type = $_FILES['image']['type'];
-        $img_nom  = $_FILES['image']['name'];
-        $auteur   = GetSQLValue("SELECT pseudo FROM compte WHERE id_compte ='$id_compte'");
-        $id_article   = GetSQLValue("SELECT id_article FROM article WHERE nom_article ='$nom_article'");
-        echo "$nom_article";
-        
-        // Correction : Utilisation de 'image' comme clé
-        $img_blob = file_get_contents($_FILES['image']['tmp_name']);
-        
-        // Correction de l'insertion avec 'mysqli_query'
-        $sql = "update article set image='" . addslashes($img_blob) . "' where auteur='$auteur'";
+            if ($img_taille > $taille_max) 
+            {
+                echo "Fichier trop volumineux";
+                return false;
+            }
     
+            $img_type = $_FILES['image']['type'];
+            $img_nom  = $_FILES['image']['name'];
+            $auteur   = GetSQLValue("SELECT pseudo FROM compte WHERE id_compte ='$id_compte'");
+            $id_article   = GetSQLValue("SELECT id_article FROM article WHERE nom_article ='$nom_article'");
+            echo "$nom_article";
+            
+            // Correction : Utilisation de 'image' comme clé
+            $img_blob = file_get_contents($_FILES['image']['tmp_name']);
+            
+            // Correction de l'insertion avec 'mysqli_query'
+            $sql = "update article set image='" . addslashes($img_blob) . "' where auteur='$auteur'";
         
-        // Utilisation de mysqli_query à la place de mysql_query
-        $ret = mysqli_query($link, $sql) or die(mysqli_error($link));
-        return true;
+            
+            // Utilisation de mysqli_query à la place de mysql_query
+            $ret = mysqli_query($link, $sql) or die(mysqli_error($link));
+            return true;
         }
-        ExecuteSQL($sql1);
+        ExecuteSQL($sql);
     }
     
    
