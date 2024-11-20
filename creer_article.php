16d952214ad85140b8adfd1aeaa888360e7fc435
ColineTournillon <132445584+ColineTournillon@users.noreply.github.com>
2024-11-20 14:59:56 +0100

    Update creer_article.php
    toujours pour un bouton

--- creer_article.php
+++ creer_article.php
@@ -29,10 +29,8 @@
     <label for="image">Choisissez une image :</label>
     <input type="file" name="image"> 
     <br>
-    <form action="ajouter_pdf.php" method="POST" enctype="multipart/form-data">
     <label for="file">Choisissez un fichier PDF :</label>
     <input type="file" name="file" id="file" accept="application/pdf" required>
-    </form>
     </br>
     <input type="submit" value="Valider">
     </form>
