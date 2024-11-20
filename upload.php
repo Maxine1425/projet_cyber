f94e71ae88b7e32a46707ddfd534c29b38b108d5
ColineTournillon <132445584+ColineTournillon@users.noreply.github.com>
2024-11-20 14:53:17 +0100

    Update upload.php
    plus qu'un fichier

--- upload.php
+++ upload.php
@@ -60,6 +60,43 @@ if (isset($_POST["nom_article"]))
     exit();
 }
 ?>
+<?php
+
+    require 'config.php';
+    EstConnecte();
+    if (isset($_POST['import'])) {
+
+        // Vérification du fichier
+        if (isset($_FILES['file']) && $_FILES['file']['error'] === UPLOAD_ERR_OK) {
+            $fileName = $_FILES['file']['name'];
+            $fileTmpPath = $_FILES['file']['tmp_name'];
+            $fileType = $_FILES['file']['type'];
+
+            // Vérifier que le fichier est un PDF
+            if ($fileType === 'application/pdf') {
+                $fileContent = file_get_contents($fileTmpPath);
+
+                // Préparer la requête
+                $sql = prepare("INSERT INTO pdf (name, file) VALUES (?, ?)");
+                mysqli_bind_param($sql,"sb", $fileName, $fileContent);
+                
+                if (ExecuteSQL($sql)) {
+                    echo "Fichier téléchargé et enregistré avec succès !";
+                } else {
+                    echo "Erreur lors de l'enregistrement du fichier : " . $sql->error;
+                }
+
+            } else {
+                echo "Veuillez importer uniquement des fichiers PDF.";
+            }
+        } else {
+            echo "Erreur lors de l'import du fichier.";
+        }
+
+    }
+    // ExecuteSQL($sql);
+?>
+
 
 <!DOCTYPE html>
 <html lang="en">
