c4ce41643c142229ada24bcf295cd709e8cd374e
Maxine1425 <105820818+Maxine1425@users.noreply.github.com>
2024-11-27 15:25:18 +0100

    Update config.php

--- config.php
+++ config.php
@@ -2,10 +2,10 @@
 session_start();
 
 $host='localhost';
-//$user = $_SERVER['USER_NAME'];
-//$password = $_SERVER['MDP_BDD'];
-$user = "maco";
-$password = "l'ec'efmtnc'ePD33";
+$user = $_SERVER['USER_NAME'];
+$password = $_SERVER['MDP_BDD'];
+//$user = "maco";
+//$password = "l'ec'efmtnc'ePD33";
 $base = "projet_cybermaco";
 
 $link = connexion_MySQLi_procedural($host,$user,$password,$base);
